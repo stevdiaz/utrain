0e0a8849b91ce298840d5d8cb46fb0f7d95d8707
Steven Diaz <stevdiaz@mit.edu>
2020-01-17 13:16:51 -0500

    Added google client id

--- client/src/components/pages/Skeleton.js
+++ client/src/components/pages/Skeleton.js
@@ -5,7 +5,7 @@ import "../../utilities.css";
 import "./Skeleton.css";
 
 //TODO: REPLACE WITH YOUR OWN CLIENT_ID
-const GOOGLE_CLIENT_ID = "121479668229-t5j82jrbi9oejh7c8avada226s75bopn.apps.googleusercontent.com";
+const GOOGLE_CLIENT_ID = "913047107914-lv5bn822famjelijhv0nipk9q8vqie36.apps.googleusercontent.com";
 
 class Skeleton extends Component {
   constructor(props) {
--- server/auth.js
+++ server/auth.js
@@ -4,7 +4,7 @@ const socket = require("./server-socket");
 
 // create a new OAuth client used to verify google sign-in
 //    TODO: replace with your own CLIENT_ID
-const CLIENT_ID = "121479668229-t5j82jrbi9oejh7c8avada226s75bopn.apps.googleusercontent.com";
+const CLIENT_ID = "913047107914-lv5bn822famjelijhv0nipk9q8vqie36.apps.googleusercontent.com";
 const client = new OAuth2Client(CLIENT_ID);
 
 // accepts a login token from the frontend, and verifies that it's legit
